43019c65bcaabaaf5b0978b1d849f7c087040968
Armando Ocampo <aocampo@mymail.mines.edu>
2019-11-17 14:00:24 -0700

    Finished step2: LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,11 @@
 def PrintOutput(string):
     print("OUTPUT", string)
+    
+def LoadFile(path):
+    lines = []
+    iFile = open(path, "r")
+    for line in iFile:
+        lines.append(line.rstrip("\n\r"))
+    return(lines)
+
+#PrintOutput(LoadFile("../sample_file.txt"))
