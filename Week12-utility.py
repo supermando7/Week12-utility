1b311dc2d60e10738fa48f09d0052201ea85b834
Armando Ocampo <aocampo@mymail.mines.edu>
2019-11-17 15:51:38 -0700

    Finished step5: ScoreFinder

--- Week12-utility.py
+++ Week12-utility.py
@@ -16,16 +16,29 @@ def UpdateString(str1, str2, index):
         finalStr += chars
     PrintOutput(finalStr)
 
-def FindWordCount(lst, s):
+def FindWordCount(lines, s):
     counter = 0
-    for line in lst:
+    for line in lines:
         line = line.split(" ")
         for word in line:
             if word == s:
                 counter += 1
     return counter
 
+def ScoreFinder(players, scores, targetName):
+    index = 0
+    for tempName in players:
+        if(tempName.lower() == targetName.lower()):
+            PrintOutput(str(players[index]) + " got a score of " + str(scores[index]))
+            return
+        index += 1
+    PrintOutput("player not found")
+
 #PrintOutput(LoadFile("../sample_file.txt"))
 #UpdateString("Hello World", "a", 3)
 #a = LoadFile("../sample_file.txt")
 #PrintOutput(str(FindWordCount(a, "How")))
+#players = ["Mary", "Cody", "Joe", "Jill", "Xai", "Bodo"]
+#scores = [5, 8, 10, 6, 10, 4]
+#ScoreFinder(players, scores, "jill")
+#ScoreFinder(players, scores, "Manuel")
